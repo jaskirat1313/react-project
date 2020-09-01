73fef874a5779bcbda0a8ff3b986f9da324d4d6c
jaskirat1313 <jaskiratsingh568@gmail.com>
2020-09-02 01:32:16 +0530

    first commit react

--- src/comon.jsx
+++ src/comon.jsx
@@ -8,10 +8,11 @@ const Comon=(props)=> {
     
       <>
       <section id="header" className="d-flex align-items-center">
-                <div className="container-fluid mb-auto">
+        <div className="container-fluid ">
           <div className="row">
-              <div className="col-10 mx-auto  d-flex">
-                  <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
+            <div className="col-10 mx-auto">
+              <div className="row">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
                       <h1>{props.title}<strong className="brand-name"> Jaskirat Singh</strong></h1>
                       <h2 className="my-3">{props.des}</h2>
                      <div className="mt-3">
@@ -19,7 +20,7 @@ const Comon=(props)=> {
                          
                     <NavLink to={props.link}  className="btn-get-started">{props.button}</NavLink> </div>
                   </div>
-                  <div className="col-lg-6 order-1 order-lg-2 header_img">
+                  <div className="col-lg-6 order-1 order-lg-2 header-img">
                       <img className="img-fluid animated" src={props.img} alt="textimage"/>
                   </div>
 
