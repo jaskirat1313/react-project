96150ec029f5a3315f836e3d6d6193b96bfa2128
jaskirat1313 <jaskiratsingh568@gmail.com>
2020-09-02 00:48:08 +0530

    Updates

---
+++ precache-manifest.d4de32fa330da2e5207fc3a244bb4ab9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ec6c6fd5b070db5b861fe1fae63de8",
+    "url": "/.github.io/react-project/index.html"
+  },
+  {
+    "revision": "9b7a9de1ec7308a32206",
+    "url": "/.github.io/react-project/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "084af2e2127e2274f10a",
+    "url": "/.github.io/react-project/static/css/main.d66bb1ef.chunk.css"
+  },
+  {
+    "revision": "9b7a9de1ec7308a32206",
+    "url": "/.github.io/react-project/static/js/2.57ecb406.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/.github.io/react-project/static/js/2.57ecb406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "084af2e2127e2274f10a",
+    "url": "/.github.io/react-project/static/js/main.b8c545d9.chunk.js"
+  },
+  {
+    "revision": "90876fcf75cfccbae0fd",
+    "url": "/.github.io/react-project/static/js/runtime-main.37645739.js"
+  },
+  {
+    "revision": "a17d5e9eb25275c64226ac00bce569de",
+    "url": "/.github.io/react-project/static/media/team.a17d5e9e.svg"
+  },
+  {
+    "revision": "7dcdb22d588092371857a4045c4cb1ec",
+    "url": "/.github.io/react-project/static/media/web.7dcdb22d.svg"
+  }
+]);
